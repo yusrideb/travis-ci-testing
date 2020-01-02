94736547c8d0531ef92d18198f310c76909545d0
Achmad Yusri Afandi <linuxer08@gmail.com>
2020-01-03 02:13:38 +0800

    add example mariadb connection

--- lib/PerlTravisCI/Testing.pm
+++ lib/PerlTravisCI/Testing.pm
@@ -1,12 +1,33 @@
 package PerlTravisCI::Testing;
 use Mojo::Base 'Mojolicious';
 
+use Mojo::mysql;
+use CellBIS::SQL::Abstract;
+
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
   # Load configuration from hash returned by config file
   my $config = $self->plugin('Config');
+  my $mysql_cfg = $config->{mysql};
+  
+  # Set Default TimeZone
+  $ENV{TZ} = $config->{timezone};
+  
+  # Helper for SQL Abstract
+  $self->helper(sql => sub { state $sql_abstract = CellBIS::SQL::Abstract->new() });
+  
+  # Helper for MySQL
+  $self->helper(
+    mysql => sub {
+      state $mysql = Mojo::mysql->new($mysql_cfg);
+    }
+  );
+  
+  # Migrate to latest version if necessary
+  my $tbl_basic  = $self->home->rel_file('migrations/main.sql');
+  $self->mysql->migrations->name('main-db')->from_file($tbl_basic)->migrate;
 
   # Configure the application
   $self->secrets($config->{secrets});
--- lib/PerlTravisCI/Testing/Controller/Example.pm
+++ lib/PerlTravisCI/Testing/Controller/Example.pm
@@ -1,12 +1,28 @@
 package PerlTravisCI::Testing::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Mojo::Util 'dumper';
+
 # This action will render a template
 sub welcome {
   my $self = shift;
 
+  my $sql = $self->app->sql;
+  my $q   = $sql->select('users', [], {where => 'username = ?'});
+
+  my $data_user = {};
+  my ($username, $fullname) = 'Unknown';
+  my $qsql = $self->mysql->db->query($q, 'yusrideb');
+  if ($qsql->rows) {
+    $data_user = $qsql->hashes;
+    $username  = $data_user->to_array->[0]->{username};
+    $fullname  = $data_user->to_array->[0]->{fullname};
+  }
+
+  my $content = "Current user : $username with fullname : $fullname";
+
   # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!', content_msg => $content);
 }
 
 1;
